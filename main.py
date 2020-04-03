b3efca5481da8e408bbd6a86e98ca039ec479db7
sungrauzis <kristers.krums@grobinasskola.lv>
2020-04-03 21:51:41 +0000

    saitins

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+app = Flask('/')
+
+
+@app.route('/')
+def home():
+    return render_template('galvenais.html')
+
+
+@app.route('/viens')
+def viens():
+    return render_template('viens.html')
+
+
+@app.route('/divi')
+def divi():
+    return render_template('divi.html')
+
+
+@app.route('/')
+def galvenais():
+    return render_template('galvenais.html')
+
+@app.route('/galerija')
+def galerija():
+    return render_template('galerija.html')
+
+if __name__ == "__main__":
+  app.run('0.0.0.0', port=8020)
+#app.run('0.0.0.0', port=8020)
